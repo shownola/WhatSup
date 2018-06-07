451d15918c3927b783a3e3506e57e3af4bf4638b
SHERRY <swasieko@gmail.com>
2018-06-07 23:43:42 +0000

    Add new recipe creations with tests

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -17,8 +17,42 @@ class RecipesTest < ActionDispatch::IntegrationTest
   test 'should get recipes listing' do
     get recipes_path
     assert_template 'recipes/index'
-    assert_match @recipe.name, response.body
-    assert_match @recipe2.name, response.body
+    assert_select 'a[href=?]', recipe_path(@recipe), text: @recipe.name
+    assert_select 'a[href=?]', recipe_path(@recipe2), text: @recipe2.name
+  end
+  
+  # test 'should get recipe show' do
+  #   get recipe_path(@recipe)
+  #   assert_template 'recipes/show'
+  #   assert_match @recipe.name.capitalize, response.body
+  #   assert_match @recipe.directions, response.body
+  #   assert_match @chef.chefname, response.body
+  # end
+  
+  test 'create new valid recipe' do
+    get new_recipe_path
+    assert_template 'recipes/new'
+    name_of_recipe = 'pepper chicken'
+    directions_of_recipe = 'add pepper then cook for 5 minutes'
+    assert_difference 'Recipe.count', 1 do
+      post recipes_path, params: {recipe: { name: name_of_recipe, directions: directions_of_recipe } }
+    end
+    follow_redirect!
+    assert_match name_of_recipe.titleize, response.body
+    assert_match directions_of_recipe, response.body
+  end
+  
+  test 'reject invalid recipe submissions' do
+    get new_recipe_path
+    assert_template 'recipes/new'
+    assert_no_difference 'Recipe.count' do
+      post recipes_path, params: { recipe: { name: " ", directions: " " } }
+    end
+    assert_template 'recipes/new'
+    assert_select 'h2.card-title'
+    assert_select 'div.card-body'
   end
   
 end
+
+
