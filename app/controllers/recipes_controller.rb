4a21eadc4696a4e72ac5c993e4a981ecf8dd61c9
SHERRY <swasieko@gmail.com>
2018-06-08 04:39:15 +0000

    Refactored code

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,12 @@
 class RecipesController < ApplicationController
+  before_action :find_recipe, only: [:show, :edit, :update]
   
   def index
     @recipes = Recipe.all
   end
   
   def show
-    @recipe = Recipe.find(params[:id])
+   
   end
   
   def new
@@ -24,11 +25,11 @@ class RecipesController < ApplicationController
   end
   
   def edit
-    @recipe = Recipe.find(params[:id])
+   
   end
   
   def update
-    @recipe = Recipe.find(params[:id])
+   
     if @recipe.update(recipe_params)
       flash[:success] = "Recipe has been updated"
       redirect_to recipe_path(@recipe)
@@ -45,9 +46,15 @@ class RecipesController < ApplicationController
   
   private
   
+     def find_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+  
     def recipe_params
       params.require(:recipe).permit(:name, :directions)
     end
+    
+   
   
 end
 
