16d214fc58a8b80d5778a32dd59389c913e9446a
SHERRY <swasieko@gmail.com>
2018-06-12 04:21:11 +0000

    Get app production ready with action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://shownola-what-sup.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://shownola-what-sup.herokuapp.com', 'http://shownola-what-sup.herokuapp.com/']
 end
